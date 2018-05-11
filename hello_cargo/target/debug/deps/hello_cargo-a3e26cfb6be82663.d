e6b336274f1e2d369ff772b71d92584e3738b2b3
Ben Ramsden <br1@intranet.datcon.co.uk>
2018-05-11 12:06:57 +0100

    hello_cargo project

---
+++ hello_cargo/target/debug/deps/hello_cargo-a3e26cfb6be82663.d
@@ -0,0 +1,5 @@
+/home/br1/rust_book/hello_cargo/target/debug/deps/hello_cargo-a3e26cfb6be82663: src/main.rs
+
+/home/br1/rust_book/hello_cargo/target/debug/deps/hello_cargo-a3e26cfb6be82663.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/br1/rust_book/hello_cargo/target/debug/hello_cargo: /home/br1/rust_book/hello_cargo/src/main.rs
